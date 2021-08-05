3b573a5eb81fb7e9f62de81fdd168e35968d487a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-06 05:36:59 +0900

    データ取得クリックでaxiosからデータ取得して表示（型定義も）

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import { UserCard } from './components/UserCard';
-
-const user = {
-  id: 1,
-  name: "てけ",
-  email: "teke@teke.com",
-  address: "ADDRESS",
-}
+import axios from 'axios';
+import { User } from './types/api/user';
+import { UserProfile } from './types/userProfile';
 
 function App() {
+  const [ userProfiles, setUserProfiles ] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) => {
+      const data = res.data.map((user) => ({
+        id: user.id,
+        name: `${user.name}（${user.username}）`,
+        email: user.email,
+        address: `${user.address.city}${user.address.suite}${user.address.street}`
+      }));
+      setUserProfiles(data);
+    });
+  };
   return (
     <div className="App">
-      <UserCard user={user}/>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user}/>
+      ))}
     </div>
   );
 }
