738600c87c3c200af4563f0411b75fdddab4a54a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-05 22:50:59 +0900

    UserCard土台作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { UserCard } from './components/UserCard';
+
+const user = {
+  id: 1,
+  name: "てけ",
+  email: "teke@teke.com",
+  address: "ADDRESS",
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserCard user={user}/>
     </div>
   );
 }
